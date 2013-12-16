c6494e9c0e13b8ee564672b3dcb9d92b954b5e81
Antonio López Martínez <antonio.lopmar@gmail.com>
2013-12-16 00:58:05 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Namox::Application.config.secret_key_base = 'e1a7ec6dfd5c291886436ca0d14687ed4049bee28c7eab89ecc8632e663029c78762d12a5a7dce77db8664e64a48d8d5bc6bd003e28a46650f1d9ccd2c46000b'
